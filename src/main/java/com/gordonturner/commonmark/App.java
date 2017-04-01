18e81dd5e2dd67ea6c43c08de94965e0e103f8be
Gordon Turner <gordon.turner@gmail.com>
2017-03-31 20:53:30 -0400

    Working file read write and executable jar.

--- src/main/java/com/gordonturner/commonmark/App.java
+++ src/main/java/com/gordonturner/commonmark/App.java
@@ -1,13 +1,42 @@
 package com.gordonturner.commonmark;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+  public static void main(String[] args)
+  {
+    
+    try
     {
-        System.out.println( "Hello World!" );
+      FileReader fileReader = new FileReader(args[0]);
+      FileWriter fileWriter = new FileWriter(args[0] + ".html");
+      
+      Parser parser = Parser.builder().build();
+      Node document = parser.parseReader(fileReader);
+      HtmlRenderer renderer = HtmlRenderer.builder().build();
+      String rendered = renderer.render(document);
+      
+      System.out.println( rendered );
+      fileWriter.write(rendered);
+      fileWriter.close();
+      
     }
+    catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    } catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 }
